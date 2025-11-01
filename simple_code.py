66e8c57b695bba20cc9b6078bd67290d2820288b
ssokolova01 <ssokolova01@qub.ac.uk>
2025-11-01 19:05:08 +0000

    simple_code.py

--- simple_code.py
+++ simple_code.py
@@ -14,7 +14,7 @@ data = {
 }
 
 #Converting the dictionary to Dataframe with custom row names for better clarity
-df = pd.DataFrame(data, index = ["Member 1", "Member 2", "Member 3","Member 4","Member 5","Member 6"])
+df = pd.DataFrame(data, index = ["Member 1", "Member 2", "Member 3","Member 4","Member 5","Member 6", "Memeber 7"])
 
 
 #Saving the dataframe as a csv file
