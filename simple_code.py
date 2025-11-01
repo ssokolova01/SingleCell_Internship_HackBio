6b75b6e33cc63b164891377954c55e8d29e97aaa
ssokolova01 <ssokolova01@qub.ac.uk>
2025-11-01 19:04:27 +0000

    simple_code.py

--- simple_code.py
+++ simple_code.py
@@ -5,12 +5,12 @@ import pandas as pd
 
 #Creating dictionary with Column names (key) and Adding Member information next to each Column name (Values)
 data = {
-  "Name": ["Svetlana","Santosh","Debangana","Gideon Danso","Mosunmola Temitope Christianah","Hana E"],
-  "Slack Username": ["Svetlana Sokolova","Santosh T","Debangana","Joegidi4real","Temmy","Hana E"],
-  "Country" :["United Kingdom","United States of America","United Kingdom","Ghana","Nigeria","Canada"],
-  "Hobby":["Playing Piano","Tennis","Badminton","Listening to music","Cooking","Hiking"],
-  "Affiliation":["Queens University Belfast","University of Texas at San Antonio","Queens University Belfast (Alumna)","University of Cape Coast","Obafemi Awolowo University","Boise State University"],
-  "Favourite Gene (gene name)":["ctcaaaagtc","attctaactg","agaggcggag","agtccccgct","ctaggcggcg","gccagttctc"]
+  "Name": ["Svetlana","Santosh","Debangana","Gideon Danso","Mosunmola Temitope Christianah","Hana E", "Adewumi Esther Adeola"],
+  "Slack Username": ["Svetlana Sokolova","Santosh T","Debangana","Joegidi4real","Temmy","Hana E", "adewumiesther"],
+  "Country" :["United Kingdom","United States of America","United Kingdom","Ghana","Nigeria","Canada", "Nigeria"],
+  "Hobby":["Playing Piano","Tennis","Badminton","Listening to music","Cooking","Hiking", "Cooking"],
+  "Affiliation":["Queens University Belfast","University of Texas at San Antonio","Queens University Belfast (Alumna)","University of Cape Coast","Obafemi Awolowo University","Boise State University", "Obafemi Awolowo University"],
+  "Favourite Gene (gene name)":["ctcaaaagtc","attctaactg","agaggcggag","agtccccgct","ctaggcggcg","gccagttctc", "ctcaaaagtc"]
 }
 
 #Converting the dictionary to Dataframe with custom row names for better clarity
