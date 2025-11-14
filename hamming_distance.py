371e9c9bd41fceaacb1b9de17458e57eaa7a0ad3
ssokolova01 <ssokolova01@qub.ac.uk>
2025-11-14 15:03:35 +0000

    Update hamming_distance.py

--- hamming_distance.py
+++ hamming_distance.py
@@ -224,7 +224,7 @@ plt.legend(handles, labels, title='significance', loc='upper right')
 # Show the plot
 plt.show()
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-Part B – Breast Cancer Data Exploration
+# Part B – Breast Cancer Data Exploration
 
 # 1. c. Scatter Plot (radius vs texture)
 # Plot texture_mean vs radius_mean and color points by diagnosis (M = malignant, B = benign).
